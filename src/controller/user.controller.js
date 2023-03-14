95648b7abf77360f95b600f42025e98b28de0bfe
jpuy22 <117951575+jpuy22@users.noreply.github.com>
2023-03-14 19:55:23 -0300

    Add files via upload

---
+++ src/controller/user.controller.js
@@ -0,0 +1,11 @@
+export const getUsers = async(req, res) => {
+    res.send({status: 'success', result: [] })
+}
+
+export const getUserByID = async(req, res) => {
+    res.send({status: 'success', result: {} })
+}
+
+export const createUser = async(req, res) => {
+    res.send({status: 'success', result: {} })
+}
